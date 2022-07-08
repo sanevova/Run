6d5bdfc70cbce1fe23faedb17def84d2b47ede3c
Vova Mishatkin <sane.vova@gmail.com>
2022-07-08 01:22:04 +0100

    simple run

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class DinoController : MonoBehaviour {
     public Rigidbody2D body;
     public Animator animator;
+    public GameObject grid;
     public float jumpSpeed;
     private bool isGrounded = false;
+    public float speedX = 1;
 
     void Start() {
+        grid.GetComponent<Rigidbody2D>().velocity = Vector2.left * speedX;
+        body.velocity = new Vector2(speedX, body.velocity.y);
     }
 
     void Update() {
+        body.velocity = new Vector2(speedX, body.velocity.y);
         if (IsInputJump() && isGrounded) {
             body.velocity = new Vector2(body.velocity.x, jumpSpeed);
         }
