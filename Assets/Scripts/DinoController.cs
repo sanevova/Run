ffc9d14676ce1f26a0407b144c4e61486e16aaf0
Vova Mishatkin <sane.vova@gmail.com>
2022-07-13 19:12:52 +0100

    decople ground and speed from dino

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -1,32 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using TMPro;
 
 public class DinoController : MonoBehaviour {
     const float TREE_SPAWN_INTERVAL_MIN = 0.8F;
     const float TREE_SPAWN_INTERVAL_MAX = 1.6F;
-    const int SPEED_INCREASE_INTERVAL_GROWTH = 50;
 
+    public GameController game;
     public Rigidbody2D body;
     public Animator animator;
     public GameObject grid;
-    public GameObject groundTilemap;
     public float jumpSpeed;
     private bool didJump = false;
     private bool didIncreaseJumpGravity = false;
     private float defaultGravity;
     public float stickyJumpGravity = 1.2f;
     private bool isGrounded = false;
-    public float speedX = 10;
     public GameObject tree;
-    public TMP_Text deathText;
-    public TMP_Text scoreText;
-    private float score = 0;
-    private int nextSpeedIncreaseScore = SPEED_INCREASE_INTERVAL_GROWTH;
-    private int speedIncreaseInterval = SPEED_INCREASE_INTERVAL_GROWTH;
-    public float speedIncreaseFactor = 1.2f;
+
     private float lastTreeSpawnTime = 0;
     private float nextTreeSpawnTime = 0;
 
@@ -36,36 +27,19 @@ public class DinoController : MonoBehaviour {
     }
 
     void Update() {
-        MoveGround();
         SpawnTrees();
         HandleInputs();
         if (didJump) {
             ApplyStickyJumpGravity();
         }
-        UpdateScore();
     }
 
     void HandleInputs() {
-        if (Input.GetButtonDown("Fire2")) {
-            // pause/unpause
-            Time.timeScale = 1 - Time.timeScale;
-            deathText.enabled = !deathText.enabled;
-            GetComponent<SpriteRenderer>().color = Color.white;
-        }
         if (Input.GetAxis("Horizontal") > 0) {
             Debug.Log(body.velocity);
         }
         if (IsInputJump() && CanJump()) {
             Jump();
-        }
-    }
-
-    void MoveGround() {
-        Tilemap tilemap = groundTilemap.GetComponent<Tilemap>();
-        tilemap.tileAnchor += Vector3.left * speedX * Time.deltaTime;
-        float x = tilemap.tileAnchor.x;
-        if (x < 0) {
-            tilemap.tileAnchor += Vector3.right * Mathf.Floor(Mathf.Abs(x) + 1);
         }
     }
 
@@ -103,7 +77,7 @@ public class DinoController : MonoBehaviour {
     void SpawnTree() {
         GameObject newTree = Instantiate(tree, tree.transform.position + Vector3.left, tree.transform.rotation);
         newTree.SetActive(true);
-        newTree.GetComponent<Rigidbody2D>().velocity = Vector2.left * speedX;
+        newTree.GetComponent<Rigidbody2D>().velocity = Vector2.left * game.speedX;
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
@@ -120,7 +94,7 @@ public class DinoController : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Target") {
             Debug.Log("HIT TREE");
-            deathText.enabled = true;
+            game.deathText.enabled = true;
             Time.timeScale = 0;
             GetComponent<SpriteRenderer>().color = Color.red;
             other.GetComponent<SpriteRenderer>().color = Color.grey;
@@ -132,19 +106,5 @@ public class DinoController : MonoBehaviour {
             isGrounded = false;
             animator.SetBool("IsGrounded", false);
         }
-    }
-
-    void UpdateScore() {
-        score += Time.deltaTime * speedX;
-        int displayScore = (int)Mathf.Floor(score);
-        if (displayScore > nextSpeedIncreaseScore) {
-            speedX *= speedIncreaseFactor;
-            foreach (GameObject tree in GameObject.FindGameObjectsWithTag("Target")) {
-                tree.GetComponent<Rigidbody2D>().velocity = Vector2.left * speedX;
-            }
-            nextSpeedIncreaseScore += speedIncreaseInterval;
-            speedIncreaseInterval += SPEED_INCREASE_INTERVAL_GROWTH;
-        }
-        scoreText.text = string.Format("score: {0}\nspeed: {1:0.0}", displayScore.ToString(), speedX);
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using TMPro;
+
+public class GameController : MonoBehaviour {
+    const int SPEED_INCREASE_INTERVAL_GROWTH = 50;
+
+    DinoController dino;
+    public float speedX = 10;
+    public GameObject groundTilemap;
+    public TMP_Text deathText;
+    public TMP_Text scoreText;
+    private float score = 0;
+    private int nextSpeedIncreaseScore = SPEED_INCREASE_INTERVAL_GROWTH;
+    private int speedIncreaseInterval = SPEED_INCREASE_INTERVAL_GROWTH;
+    public float speedIncreaseFactor = 1.2f;
+
+    void Start() {
+        dino = GameObject.FindObjectOfType<DinoController>();
+    }
+
+    void Update() {
+        ProcessInputs();
+        MoveGround();
+        UpdateScore();
+        UpdateSpeed();
+    }
+
+    void ProcessInputs() {
+        if (Input.GetButtonDown("Fire2")) {
+            // pause/unpause
+            Time.timeScale = 1 - Time.timeScale;
+            deathText.enabled = !deathText.enabled;
+            dino.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
+
+    void MoveGround() {
+        Tilemap tilemap = groundTilemap.GetComponent<Tilemap>();
+        tilemap.tileAnchor += Vector3.left * speedX * Time.deltaTime;
+        float x = tilemap.tileAnchor.x;
+        if (x < 0) {
+            tilemap.tileAnchor += Vector3.right * Mathf.Floor(Mathf.Abs(x) + 1);
+        }
+    }
+
+    void UpdateScore() {
+        score += Time.deltaTime * speedX;
+        int displayScore = (int)Mathf.Floor(score);
+        scoreText.text = string.Format("score: {0}\nspeed: {1:0.0}", displayScore.ToString(), speedX);
+    }
+
+    void UpdateSpeed() {
+        if (score < nextSpeedIncreaseScore) {
+            return;
+        }
+        speedX *= speedIncreaseFactor;
+        foreach (GameObject target in GameObject.FindGameObjectsWithTag("Target")) {
+            target.GetComponent<Rigidbody2D>().velocity = Vector2.left * speedX;
+        }
+        nextSpeedIncreaseScore += speedIncreaseInterval;
+        speedIncreaseInterval += SPEED_INCREASE_INTERVAL_GROWTH;
+    }
+}
